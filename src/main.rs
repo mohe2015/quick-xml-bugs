013fbe22ed57ef78b1c9fa5ddd73771ecc15c0a4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-08-13 22:35:40 +0200

    Initial bug commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+extern crate serde;
+use quick_xml::{DeError, de::from_str};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct NamespaceBug {
+    #[serde(rename = "xmlns:d")]
+    test: String,
+
+    #[serde(rename = "$unflatten=d:test2")] // remove "d:" and it works
+    test2: String
+}
+
+#[derive(Deserialize, Debug)]
+struct InnerNestingBug {
+}
+
+#[derive(Deserialize, Debug)]
+struct NestingBug {
+    // comment out one of these fields and it works
+    #[serde(rename = "$unflatten=outer1")]
+    outer1: InnerNestingBug,
+
+    #[serde(rename = "$unflatten=outer2")]
+    outer2: String
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let namespace_bug: Result<NamespaceBug, DeError> = from_str(r#"<?xml version="1.0" encoding="UTF-8"?><d:test xmlns:d="works"><d:test2>doesntwork</d:test2></d:test>"#);
+
+    println!("{:?}", namespace_bug);
+
+    let nesting_bug: Result<NestingBug, DeError> = from_str(r#"<?xml version="1.0" encoding="UTF-8"?><root><outer1></outer1><outer2></outer2></root>"#);
+
+    println!("{:?}", nesting_bug);
+}
